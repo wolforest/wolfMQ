e303132135ae64d27385b52063b809cc25d1afa3
思源 <winglechen@gmail.com>
2021-04-19 10:32:29 +0800

    regenerate clent related sql files

--- store-rdb/src/main/java/study/daydayup/wolf/mq/store/rdb/converter/ClientConverter.java
+++ store-rdb/src/main/java/study/daydayup/wolf/mq/store/rdb/converter/ClientConverter.java
@@ -4,39 +4,39 @@ import org.springframework.beans.BeanUtils;
 import study.daydayup.wolf.common.lang.contract.Converter;
 import study.daydayup.wolf.common.util.collection.CollectionUtil;
 import study.daydayup.wolf.mq.api.domain.entity.client.Client;
-import study.daydayup.wolf.mq.store.rdb.dataobject.MqConsumerDO;
+import study.daydayup.wolf.mq.store.rdb.dataobject.MqClientDO;
 
 import java.util.List;
 
 /**
- * study.daydayup.wolf.business.uc.crm.biz.customer.consumer.converter
+ * study.daydayup.wolf.business.uc.crm.biz.customer.client.converter
  *
  * @author Wingle
  * @since 2020/3/10 3:50 下午
  **/
 public class ClientConverter implements Converter {
-    public static Client toModel(MqConsumerDO consumerDO) {
-        if (consumerDO == null) {
+    public static Client toModel(MqClientDO clientDO) {
+        if (clientDO == null) {
             return null;
         }
 
         Client client = new Client();
-        BeanUtils.copyProperties(consumerDO, client);
+        BeanUtils.copyProperties(clientDO, client);
 
         return client;
     }
 
-    public static List<Client> toModel(List<MqConsumerDO> consumerDOList) {
-        return CollectionUtil.to(consumerDOList, ClientConverter::toModel);
+    public static List<Client> toModel(List<MqClientDO> clientDOList) {
+        return CollectionUtil.to(clientDOList, ClientConverter::toModel);
     }
 
-    public static MqConsumerDO toDO(Client client) {
+    public static MqClientDO toDO(Client client) {
         if (client == null) {
             return null;
         }
 
-        MqConsumerDO consumerDO = new MqConsumerDO();
-        BeanUtils.copyProperties(client, consumerDO);
-        return consumerDO;
+        MqClientDO clientDO = new MqClientDO();
+        BeanUtils.copyProperties(client, clientDO);
+        return clientDO;
     }
 }
